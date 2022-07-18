058c6d275e998a2cc273ca750bff48be30b109c5
James Belk <jamesbelk0@gmail.com>
2022-07-18 09:00:12 -0400

    initial push. created react app

---
+++ weather-app/src/App.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+import axios from 'axios';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
